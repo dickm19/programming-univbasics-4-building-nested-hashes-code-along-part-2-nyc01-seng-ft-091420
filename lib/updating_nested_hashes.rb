2f75be8ccd66682220edfd0e56600a3b21067d46
Mary Dick <maryclairedick@gmail.com>
2020-08-14 22:45:18 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -53,7 +53,7 @@ def add_characters
       epic_tragedy[:additional_characters] << {name: "Prince Escalus"}
       epic_tragedy[:additional_characters] << {name: "Apothecary"}
         
-   }
+   
 
    
    epic_tragedy
